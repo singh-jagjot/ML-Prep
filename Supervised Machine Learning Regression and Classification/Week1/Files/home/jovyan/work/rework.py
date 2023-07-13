8312acef3fecefbc677fa0dd2fb0e34a803f4e24
singh-jagjot <jagjot.no1@gmail.com>
2023-07-13 04:03:44 -0400

    added gradient descent

--- Supervised Machine Learning Regression and Classification/Week1/Files/home/jovyan/work/rework.py
+++ Supervised Machine Learning Regression and Classification/Week1/Files/home/jovyan/work/rework.py
@@ -31,4 +31,32 @@ def comp_gradient(x, y, w, b):
     return dj_dw, dj_db
 
 # Gradient Descent
-# def gradient_descent():
+
+
+def gradient_descent(w_in, b_in, x, y, alpha, num_of_itr):
+    w = w_in
+    b = b_in
+
+    for i in range(num_of_itr):
+        dj_dw, dj_db = comp_gradient(x, y, w, b)
+        w = w - alpha * dj_dw
+        b = b - alpha * dj_db
+
+    return w, b
+
+# Initial Parameters
+w_init = 0
+b_init = 0
+iterations = 10000
+tmp_alpha = 1.0e-2
+
+x_train = np.array([1.0, 2.0])   #features
+y_train = np.array([300.0, 500.0])   #target value
+
+w_final, b_final = gradient_descent(w_init, b_init, x_train ,y_train, tmp_alpha, iterations)
+
+print(w_final, b_final)
+
+print(f"1000 sqft house prediction {w_final*1.0 + b_final:0.1f} Thousand dollars")
+print(f"1200 sqft house prediction {w_final*1.2 + b_final:0.1f} Thousand dollars")
+print(f"2000 sqft house prediction {w_final*2.0 + b_final:0.1f} Thousand dollars")
