c71cc745a8c64700097c5a657b6c79ad1fa714cf
singh-jagjot <jagjot.no1@gmail.com>
2023-07-22 23:25:41 -0400

    +cheat sheet linear algebra

--- demo.py
+++ demo.py
@@ -11,3 +11,10 @@ model = tf.keras.applications.ResNet50(
 loss_fn = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=False)
 model.compile(optimizer="adam", loss=loss_fn, metrics=["accuracy"])
 model.fit(x_train, y_train, epochs=5, batch_size=64)
+
+# import numpy as np
+
+# a = np.array([1,2,3,4])
+# b = np.transpose(a)
+
+# print()
