256624fb585600a8652bc2d998d1faa38ada356c
singh-jagjot <jagjot.no1@gmail.com>
2023-08-17 05:51:36 -0400

    added w1 ALA

--- README.md
+++ README.md
@@ -1,4 +1,2 @@
 # coursera-prep
 Backup files for my preparation of Coursera Specializations/Courses
-My code mostly in 'rework.py' or 'practiceNUmber.py'.
-Programming Assignment code is in assignment*number*.py
--- demo.py
+++ demo.py
@@ -1,4 +1,6 @@
-import tensorflow as tf
+import sys
+import numpy as np
+# import tensorflow as tf
 
 # cifar = tf.keras.datasets.cifar100
 # (x_train, y_train), (x_test, y_test) = cifar.load_data()
@@ -12,5 +14,9 @@ import tensorflow as tf
 # model.compile(optimizer="adam", loss=loss_fn, metrics=["accuracy"])
 # model.fit(x_train, y_train, epochs=5, batch_size=64)
 
-hw = tf.config.get_visible_devices()
-tf.config.set_visible_devices(hw[0])
+# hw = tf.config.get_visible_devices()
+# tf.config.set_visible_devices(hw[0])
+
+a = np.arange(10)
+print(np.dot(a, 5))
+print()
